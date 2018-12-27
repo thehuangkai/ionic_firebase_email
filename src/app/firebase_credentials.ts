7fd5f16d8c2ef625fe228edb7a31ae8f22b7d4cc
Huang Kai <thehuangkai@users.noreply.github.com>
2018-12-27 16:31:27 +0700

    Update firebase_credentials.ts

--- src/app/firebase_credentials.ts
+++ src/app/firebase_credentials.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyDZfoHbVpE3LL_jl45RFBomk5Wx7r-w6K8",
-    authDomain: "fir-fcdd4.firebaseapp.com",
-    databaseURL: "https://fir-fcdd4.firebaseio.com",
-    projectId: "fir-fcdd4",
-    storageBucket: "fir-fcdd4.appspot.com",
-    messagingSenderId: "892100263414"
+    apiKey: "...",
+    authDomain: "...",
+    databaseURL: "...",
+    projectId: "...",
+    storageBucket: "...",
+    messagingSenderId: "..."
   };
